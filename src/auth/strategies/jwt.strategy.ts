af00afe9107fa919ff1fb1ebca2284c39c550d60
Nikitapil <nikitapiliavets@gmail.com>
2023-12-23 15:04:50 +0300

    add jwt-strategy, jwt-auth-guard, user-decorator

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,28 @@
+import process from 'process';
+import { PrismaService } from '../../prisma/prisma.service';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { UserReturnDto } from '../../dtos-global/UserReturnDto';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(private prisma: PrismaService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.ACCESS_SECRET
+    });
+  }
+
+  async validate(payload: UserReturnDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: payload.id
+      }
+    });
+    if (user) {
+      return payload;
+    }
+    return user;
+  }
+}
