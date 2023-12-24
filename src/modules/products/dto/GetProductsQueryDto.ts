09dcf238bc8216a4eb641943a66bef357f750f8e
Nikitapil <nikitapiliavets@gmail.com>
2023-12-24 22:07:02 +0300

    add GetProductsQueryDto description

---
+++ src/modules/products/dto/GetProductsQueryDto.ts
@@ -0,0 +1,38 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEnum, IsNumber, IsOptional, IsString } from 'class-validator';
+import { Transform } from 'class-transformer';
+
+export class GetProductsQueryDto {
+  @ApiProperty({ description: 'Page number', type: Number })
+  @IsNumber()
+  @Transform(({ value }) => {
+    return Number(value);
+  })
+  page: number;
+
+  @ApiProperty({ description: 'Limit number', type: Number })
+  @IsNumber()
+  @Transform(({ value }) => {
+    return Number(value);
+  })
+  limit: number;
+
+  @ApiProperty({ description: 'products categoryId', type: String })
+  @IsString()
+  @IsOptional()
+  categoryId?: string;
+
+  @ApiProperty({
+    description: 'price sorting direction',
+    oneOf: [{ type: 'asc' }, { type: 'desc' }]
+  })
+  @IsString()
+  @IsOptional()
+  @IsEnum(['asc', 'desc'])
+  priceSorting?: 'asc' | 'desc';
+
+  @ApiProperty({ description: 'search query', type: String })
+  @IsString()
+  @IsOptional()
+  search?: string;
+}
