898de617d6d4b23a79b836281931ef40538e993e
Nikitapil <nikitapiliavets@gmail.com>
2024-02-01 23:02:09 +0300

    fix delete file error

--- src/utils/files.ts
+++ src/utils/files.ts
@@ -45,7 +45,7 @@ export const getFileParsePipeWithTypeValidation = (fileType: string) => {
 
 export const deleteFile = (link: string) => {
   try {
-    const filePath = path.join(__dirname, '..', 'static', link);
+    const filePath = path.join(__dirname, '..', '..', 'static', link);
     fs.unlinkSync(filePath);
   } catch (e) {
     throw new HttpException('Error in writing file', HttpStatus.INTERNAL_SERVER_ERROR);
