5df76b5d936ada868c75691f9cf3b481b2c1ec1c
Nikitapil <nikitapiliavets@gmail.com>
2024-02-19 23:03:06 +0300

    specify CreateOrderReturnDto and RemoveFromCartDto

--- src/dtos-global/CreateOrderReturnDto.ts
+++ src/dtos-global/CreateOrderReturnDto.ts
@@ -2,9 +2,19 @@ import { OrderReturnDto } from './OrderReturnDto';
 import { CartReturnDto } from './CartReturnDto';
 import { IOrderFromDb } from '../modules/orders/types';
 import { ICartFromDb } from '../modules/cart/types';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateOrderReturnDto {
+  @ApiProperty({
+    description: 'Order object',
+    type: OrderReturnDto
+  })
   order: OrderReturnDto;
+
+  @ApiProperty({
+    description: 'updated cart after create order',
+    type: CartReturnDto
+  })
   cart: CartReturnDto;
 
   constructor(order: IOrderFromDb, cart: ICartFromDb) {
--- src/modules/cart/dto/RemoveFromCartDto.ts
+++ src/modules/cart/dto/RemoveFromCartDto.ts
@@ -1,6 +1,11 @@
 import { IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class RemoveFromCartDto {
+  @ApiProperty({
+    description: 'id of product that will be removed from cart',
+    type: String
+  })
   @IsString()
   productInCartId: string;
 }
