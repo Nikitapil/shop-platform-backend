5626a10bca499c3d2272726ede4ab8460d17d5bc
Nikitapil <nikitapiliavets@gmail.com>
2024-05-01 18:03:38 +0300

    refactor AddToCart import orders

--- src/modules/cart/dto/AddToCartDto.ts
+++ src/modules/cart/dto/AddToCartDto.ts
@@ -1,5 +1,6 @@
-import { IsString } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
+
+import { IsString } from 'class-validator';
 
 export class AddToCartDto {
   @ApiProperty({ description: 'id of product that will be added to cart', type: String })
