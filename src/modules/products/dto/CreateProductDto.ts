7c98aa408c754bb9b0eb4a305a9f64c7723c62a5
Nikitapil <nikitapiliavets@gmail.com>
2024-05-06 22:22:42 +0300

    refactor CreateProductDto

--- src/modules/products/dto/CreateProductDto.ts
+++ src/modules/products/dto/CreateProductDto.ts
@@ -1,24 +1,26 @@
-import { IsNumber, IsString } from 'class-validator';
+import { IsNumber, IsString, MinLength } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 import { Transform } from 'class-transformer';
+import { transformValueToNumber } from '../../../utils/dto-transformers';
 
 export class CreateProductDto {
   @ApiProperty({ description: 'Product name', type: String })
   @IsString()
+  @MinLength(1)
   name: string;
 
   @ApiProperty({ description: 'Product description', type: String })
   @IsString()
+  @MinLength(1)
   description: string;
 
   @ApiProperty({ description: 'Product category id', type: String })
   @IsString()
+  @MinLength(1)
   categoryId: string;
 
   @ApiProperty({ description: 'Product price', type: Number })
   @IsNumber()
-  @Transform(({ value }) => {
-    return Number(value);
-  })
+  @Transform(transformValueToNumber)
   price: number;
 }
