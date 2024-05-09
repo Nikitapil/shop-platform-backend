ce55a1925e09592cc18c643de3e57ad425869b20
Nikitapil <nikitapiliavets@gmail.com>
2024-05-09 15:30:51 +0300

    refactor app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
+import { join } from 'path';
+
 import { Module } from '@nestjs/common';
+
 import { AuthModule } from './modules/auth/auth.module';
 import { PrismaModule } from './modules/prisma/prisma.module';
 import { ProductsModule } from './modules/products/products.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 import { CategoriesModule } from './modules/categories/categories.module';
 import { CartModule } from './modules/cart/cart.module';
 import { OrdersModule } from './modules/orders/orders.module';
@@ -17,16 +19,16 @@ import { DiscountsModule } from './modules/discounts/discounts.module';
     AuthModule,
     PrismaModule,
     ProductsModule,
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'static')
-    }),
     CategoriesModule,
     CartModule,
     OrdersModule,
     ProductReviewsModule,
     FinanceModule,
     SharedModule,
-    DiscountsModule
+    DiscountsModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'static')
+    })
   ]
 })
 export class AppModule {}
